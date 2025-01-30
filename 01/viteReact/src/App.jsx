62cbed127b3d972db30b398da4a14f3932c6dadb
Atul Singh <cocatul11@gmail.com>
2025-01-30 23:52:35 +0530

    add initial Vite + React setup with App and Temp components, configuration files, and README

---
+++ 01/viteReact/src/App.jsx
@@ -0,0 +1,13 @@
+import Temp from './Temp';
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello vite + react</h1>
+      <Temp name="Atul" />
+    </>
+  )
+}
+
+export default App
---
+++ 01/viteReact/src/Temp.jsx
@@ -0,0 +1,7 @@
+export default function Test({ name }) {
+
+  return (
+    <h2>Hello, {name}</h2>
+  );
+
+}
