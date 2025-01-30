5d543af723ba8306cf0aa6c3ddf0fcbb6315c4da
Atul Singh <cocatul11@gmail.com>
2025-01-30 23:52:20 +0530

    initialize basic React app with Greetings component and setup files

---
+++ 01/basicreact/src/App.jsx
@@ -0,0 +1,12 @@
+import Greetings from "./Greetings";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello react app</h1>
+      <Greetings name={"Atul"} age={"21"} />
+    </div>
+  );
+}
+
+export default App;
