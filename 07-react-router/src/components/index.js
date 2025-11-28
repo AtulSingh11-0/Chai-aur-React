cf12cefbe75760130936766563812ac5ab839481
Atul Singh <cocatul11@gmail.com>
2025-11-28 20:42:54 +0530

    feat: create index file to export components for easier imports

---
+++ 07-react-router/src/components/index.js
@@ -0,0 +1,7 @@
+import AboutUs from "./AboutUs.jsx";
+import ContactUs from "./ContactUs.jsx";
+import Home from "./Home.jsx";
+import UserDetail, { userDetailLoader } from "./UserDetail.jsx";
+import Users from "./Users.jsx";
+
+export { AboutUs, ContactUs, Home, UserDetail, userDetailLoader, Users };
