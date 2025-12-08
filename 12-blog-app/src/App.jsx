129137b2d1114953170b3421b2a13b82979077df
Atul Singh <cocatul11@gmail.com>
2025-12-09 00:58:26 +0530

    feat: initialize 12-blog-app with Vite, React, and Tailwind CSS

---
+++ 12-blog-app/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="p-8 text-center text-6xl font-extrabold bg-amber-800 text-white">Welcome to the Blog App using Appwrite</h1>
+    </>
+  )
+}
+
+export default App;
