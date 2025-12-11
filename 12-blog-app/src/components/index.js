23f473cb18197eedb6440069e43219fc28bfcc65
Atul Singh <cocatul11@gmail.com>
2025-12-11 23:32:00 +0530

    feat: update index.js to export additional components for improved modularity

--- 12-blog-app/src/components/index.js
+++ 12-blog-app/src/components/index.js
@@ -1,4 +1,10 @@
+import Button from "./Button";
+import Container from "./container/Container";
 import Footer from "./footer/Footer";
 import Header from "./header/Header";
+import LogoutButton from "./header/LogoutButton";
+import Home from "./Home";
+import Input from "./Input";
+import Logo from "./Logo";
 
-export { Header, Footer };
+export { Button, Container, Footer, Header, Home, Input, Logo, LogoutButton };
