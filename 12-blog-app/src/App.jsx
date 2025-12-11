904d16b95006aa3f13ce77f7d21e1b30d96db7dc
Atul Singh <cocatul11@gmail.com>
2025-12-11 23:32:11 +0530

    feat: refactor App and main components for improved routing and structure

--- 12-blog-app/src/App.jsx
+++ 12-blog-app/src/App.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { login, logout } from './store/authSlice';
-import { Header, Footer } from './components';
 import { Outlet } from 'react-router';
+import { Footer, Header } from './components';
 import authService from './lib/authService';
+import { login, logout } from './store/authSlice';
 
 export default function App() {
   const [loading, setLoading] = React.useState(true);
@@ -21,8 +21,7 @@ export default function App() {
   return !loading ? (
     <>
       <Header />
-      <h1 className="p-8 text-center text-6xl font-extrabold bg-amber-800 text-white">Welcome to the Blog App using Appwrite</h1>
-      TODO: <Outlet />
+      <Outlet />
       <Footer />
     </>
   ) : null;
--- 12-blog-app/src/main.jsx
+++ 12-blog-app/src/main.jsx
@@ -1,14 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 import { Provider } from 'react-redux'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router'
+import App from './App.jsx'
+import { Home } from './components'
+import './index.css'
 import store from './store/store.js'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<App />}>
+      <Route index element={<Home />} />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>,
 )
