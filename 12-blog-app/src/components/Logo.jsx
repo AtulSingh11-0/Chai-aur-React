b99d672e5b9547e1db6a5656a52922efec86e7f0
Atul Singh <cocatul11@gmail.com>
2025-12-17 19:09:03 +0530

    feat: remove unused components and styles from the blog application

--- 12-blog-app/src/components/Logo.jsx
+++
@@ -1,6 +0,0 @@
-
-export default function Logo({ width = '100px' }) {
-  return (
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Blogger_icon_2017.svg/1024px-Blogger_icon_2017.svg.png" alt="blog-logo" style={{ width }} />
-  );
-}
